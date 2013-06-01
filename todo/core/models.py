b0f5dbb811c5e5e0558b60a9dbad36eaac581b80
scperkins <perkinsean@gmail.com>
2013-06-01 01:46:39 -0400

    set up the database for the todo list

--- todo/core/models.py
+++ todo/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+#Table name, has to wrap models.Model to get the functionality of Django.
+ class todo(models.Model):
+
+ 	name = models.CharField(max_length=100, unique=True) #like a VARCHAR field
+ 	description = models.TextField() #like a TEXT field
+ 	created = models.DateTimeField() #like a DATETIME field
+
+ 	#Tell it to return as a unicode string (The name of the to-do item) rather than just Object.
+ 	def __unicode__(self):
+ 		return self.name
